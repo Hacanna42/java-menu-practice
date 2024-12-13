166ab59bf115fb4b76579c5bf1b00816cae5c867
Hacanna42 <dtmdgus901@gmail.com>
2024-12-13 15:12:54 +0900

    feat(view): 입/출력 관련 로직 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     public static String getCoachesName() {
+        System.out.println();
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
 
         // Console.readLine() 이 없어서 Scanner로 대체
@@ -11,6 +12,7 @@ public class InputView {
     }
 
     public static String getHateMenusName(String who) {
+        System.out.println();
         System.out.println(who + "(이)가 못 먹는 메뉴를 입력해 주세요.");
         return new Scanner(System.in).nextLine();
     }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,5 +1,9 @@
 package menu.view;
 
+import java.util.List;
+import java.util.Map;
+import menu.dto.RecommendResultDto;
+
 public class OutputView {
     public static void printStartServiceMessage() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
@@ -7,5 +11,20 @@ public class OutputView {
 
     public static void printErrorMessage(String message) {
         System.out.println("[ERROR] " + message);
+    }
+
+    public static void printResult(RecommendResultDto recommendResultDto) {
+        String parsedCategories = String.join(" | ", recommendResultDto.getSelectedCategories());
+        System.out.println();
+        System.out.println("메뉴 추천 결과입니다.");
+        System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+        System.out.printf("[ 카테고리 | %s ]%n", parsedCategories);
+
+        for (Map.Entry<String, List<String>> entry : recommendResultDto.getSelectedMenusPerCoach().entrySet()) {
+            String parsedMenus = String.join(" | ", entry.getValue());
+            System.out.printf("[ %s | %s ]%n", entry.getKey(), parsedMenus);
+        }
+
+        System.out.println("\n추천을 완료했습니다.");
     }
 }
