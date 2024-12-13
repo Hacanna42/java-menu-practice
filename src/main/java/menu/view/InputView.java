4a2e9aa07d7641a01788bbec55edf019336a3397
Hacanna42 <dtmdgus901@gmail.com>
2024-12-13 13:35:15 +0900

    feat(InputView): 입력 기능 추가

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,17 @@
+package menu.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static String getCoachesName() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+
+        // Console.readLine() 이 없어서 Scanner로 대체
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static String getHateMenusName(String who) {
+        System.out.println(who + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+        return new Scanner(System.in).nextLine();
+    }
+}
